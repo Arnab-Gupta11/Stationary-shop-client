93dc8c2d3f33cb3890e771a21b7bdfaf39b50fac
Arnab Gupta <arnabgupta1111@gmail.com>
2025-04-13 21:55:26 +0600

    Add page for all deleted cateogry and show the data

--- src/pages/Dashboard/Admin/categories/DeletedCategories.tsx
+++ src/pages/Dashboard/Admin/categories/DeletedCategories.tsx
@@ -1,5 +1,102 @@
+import { CustomTable } from "../../shared/CustomTable";
+import DashboardPageSection from "../../shared/DashboardPageSection";
+import { ColumnDef } from "@tanstack/react-table";
+import { useGetAllDeletedCategoriesQuery } from "@/redux/features/categories/categories.api";
+import { TCategory } from "@/types/category.types";
+import TableSkeletonLoader from "@/components/shared/loader/table-skeleton-loader/TableSkeletonLoader";
+import { PaginationProduct } from "@/pages/AllProducts/Pagination";
+import { TMeta } from "@/types/global";
+import { useState } from "react";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { BsThreeDots } from "react-icons/bs";
 const DeletedCategories = () => {
-  return <div>DeletedCategories</div>;
+  //Hooks
+  const [page, setPage] = useState(1);
+
+  //Categories
+  const {
+    data: categoryData,
+    isLoading,
+    isFetching,
+  } = useGetAllDeletedCategoriesQuery([
+    { name: "page", value: page },
+    { name: "limit", value: 4 },
+  ]);
+  const columns: ColumnDef<TCategory>[] = [
+    {
+      header: "Icon",
+      cell: ({ row }) => (
+        <div className="flex items-center space-x-3">
+          <img
+            src={row.original.icon}
+            alt={row.original.name}
+            className="w-12 h-12 md:w-16 md:h-16 bg-light-muted-bg dark:bg-dark-muted-bg p-2 rounded-2xl flex-shrink-0 object-contain"
+          />
+        </div>
+      ),
+    },
+    {
+      accessorKey: "name",
+      header: "Name",
+    },
+    {
+      accessorKey: "description",
+      header: "Description",
+    },
+    {
+      header: "Category Type",
+      cell: ({ row }) => {
+        const isRoot = row.original.parent === null;
+        return (
+          <span
+            className={`inline-block px-3 py-1 text-sm font-medium rounded-full whitespace-nowrap ${
+              isRoot ? "bg-purple-100 text-purple-800" : "bg-yellow-100 text-yellow-800"
+            }`}
+          >
+            {isRoot ? "Parent Category" : "Sub Category"}
+          </span>
+        );
+      },
+    },
+    {
+      accessorKey: "action",
+      header: () => <div>Action</div>,
+      cell: ({ row }) => (
+        <DropdownMenu>
+          <DropdownMenuTrigger className="outline-none flex items-center justify-center hover:scale-105 active:scale-95 duration-700">
+            <BsThreeDots className="mt-2 text-xl" />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent side="bottom" className=" flex flex-col">
+            <span
+              // onClick={() => handleDelete(row.original)}
+              className="cursor-pointer flex items-center hover:text-primary-bg hover:bg-light-muted-bg dark:hover:bg-dark-muted-bg py-1 rounded-xl hover:text-primary px-3"
+            >
+              Delete
+            </span>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      ),
+    },
+  ];
+
+  return (
+    <div>
+      <DashboardPageSection>
+        <div className="mb-5 flex flex-col xs:flex-row items-center xs:justify-between gap-5">
+          <h1 className="text-lg text-light-primary-text dark:text-dark-primary-txt font-bold">Deleted Categories</h1>
+        </div>
+        {isLoading && <TableSkeletonLoader />}
+        {!isLoading && (
+          <>
+            <CustomTable columns={columns} data={categoryData?.data || []} isFetching={isFetching} />
+            <div className="mt-6 flex w-full justify-start">
+              {categoryData?.data && <PaginationProduct meta={categoryData?.meta as TMeta} page={page} setPage={setPage} />}
+            </div>
+          </>
+        )}
+      </DashboardPageSection>
+    </div>
+  );
 };
 
 export default DeletedCategories;
